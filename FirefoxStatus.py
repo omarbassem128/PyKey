bc4d83d7c6f74c13a00592f6dd955467006fe618
Omar Bassem <omarbassem889@gmail.com>
2025-03-20 17:31:29 +0200

    chose NSSM and added things in to-do

--- FirefoxStatus.py
+++ FirefoxStatus.py
@@ -1,8 +1,9 @@
 import psutil, time
 """ from pynput import keyboard """
-while True:
-    firefoxstatus = "firefox.exe" in (i.name() for i in psutil.process_iter())
-    if(firefoxstatus):
-        print(True)
-    else:
-        print(False)
+
+firefoxstatus = "firefox.exe" in (i.name() for i in psutil.process_iter())
+
+if(firefoxstatus):
+    print(True)
+else:
+    print(False)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from AppOpener import open,close
 import psutil
+import time
 
-firefoxstatus = "firefox.exe" not in (i.name() for i in  psutil.process_iter())
 
-if(firefoxstatus):
-    #compare search results here
-    open('firefox')
-    
+
+notepadstatus = "notepad.exe" not in (i.name() for i in psutil.process_iter())
+if(notepadstatus):
+    open("notepad")
+else:
+    close("notepad")
+time.sleep(5)
