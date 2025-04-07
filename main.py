9f21cb1b6759c8d7aeb24c0f76e3123200a6c6a4
Omar Bassem <omarbassem889@gmail.com>
2025-04-08 00:24:46 +0200

    need to discover how I can stop the listener once firefox closes.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from AppOpener import open,close
+""" from AppOpener import open,close """
+from pynput import keyboard
 import psutil
 import time
 
+def on_key_press(key):
+    print(f'{key} is pressed')
 
 while(True):
     firefoxstatus = "firefox.exe" in (i.name() for i in psutil.process_iter())
     if(firefoxstatus):
-        close("firefox")
-    time.sleep(4)
+        with keyboard.Listener(on_press=on_key_press) as listener:
+            listener.join()
