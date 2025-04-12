469b49b9aab92f269d17a4901c12aed05f9d5289
Omar Bassem <omarbassem889@gmail.com>
2025-04-13 00:10:01 +0200

    need to discover how to stop listener once firefox closes

--- trying_pynput_keyboard_listening.py
+++ trying_pynput_keyboard_listening.py
@@ -1,15 +1,18 @@
 from pynput import keyboard
+import time
 
-print('Program Is Running')
+print('Program Is Running, enter ctrl+c to stop')
 
 
 def on_press(key):
     try:
         print(f'{key} is pressed')
-    except AttributeError:
-        print(f'Special Key:{key} is pressed')
+        time.sleep(2)
+    except KeyboardInterrupt:
+        print("Interrupt detected. exited gracefully")
+    
         
         
 with keyboard.Listener(on_press=on_press) as listener:
-    listener.start()
+    listener.join()
     
