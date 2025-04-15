bc4faa24243e735ffb68f997fd7175487da4019f
Omar Bassem <omarbassem889@gmail.com>
2025-04-15 16:42:31 +0200

    listener is set up correctly. Up next, analyzing search queries per enter key press.

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from pynput import keyboard
 import psutil
 import time
 
-def on_key_press(key):
-    print(f'{key} is pressed')
-
-while(True):
+def checkStatus():
     firefoxstatus = "firefox.exe" in (i.name() for i in psutil.process_iter())
     if(firefoxstatus):
-        with keyboard.Listener(on_press=on_key_press) as listener:
-            listener.join()
+        return True
+    else:
+        return False
+
+def on_key_press(key):
+    checkstatus = checkStatus()
+    if(checkstatus):
+        print(f'{key} is pressed')
+
+
+with keyboard.Listener(on_press=on_key_press) as listener:
+    listener.join()
